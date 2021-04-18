5d8cc7966abf61b02e24907186b764f7da4a2750
Kevin Leung <kevinresol@gmail.com>
2021-04-18 13:32:01 +0800

    Update CopyMessageOptions

--- src/telegram/bot/types/CopyMessageOptions.hx
+++ src/telegram/bot/types/CopyMessageOptions.hx
@@ -1,6 +1,6 @@
 package telegram.bot.types;
 
-typedef CopyMessageOptions = {
+typedef CopyMessageOptions = SendBasicOptions & {
 	final chat_id : Int;
 	final from_chat_id : Int;
 	final message_id : Int;
@@ -8,7 +8,4 @@ typedef CopyMessageOptions = {
 	final ?parse_mode : ParseMode;
 	final ?caption_entities : Array<MessageEntity>;
 	final ?allow_sending_without_reply : Bool;
-	final ?disable_notification : Bool;
-	final ?reply_to_message_id : Int;
-	// final ?reply_markup : ts.AnyOf4<InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply>;
-};
+}
